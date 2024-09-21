eb6ac4834770c144edc1c84bfa1bae9cab8027e6
GD Hiruna <hirunaofficial@gmail.com>
2024-09-21 08:33:21 +0530

    Updated header.php to include navigations

--- header.php
+++ header.php
@@ -25,53 +25,32 @@
                     <div class="row align-items-center">
                         <div class="col-md-3 col-lg-2 d-none d-md-block">
                             <div class="hs-logo">
-                                <a href="index.html"><img src="images/logo/hair-salon.png" alt="hair salon logo"></a>
+                                <a href="index.php"><img src="images/logo/hair-salon.png" alt="hair salon logo"></a>
                             </div>
                         </div>
                         <div class="col-md-9 col-lg-10">
                             <div class="hs-header-top">
                                 <div class="header-top-left">
-                                    <p>Opening Hours: <span>Mon-Fri:     8.00 to 18.00</span></p>
+                                    <p>Opening Hours: <span>Mon-Fri:     8.00 to 20.00</span></p>
                                 </div>
                                 <ul class="hs-social-icon">
                                     <li><a href="#"><i class="zmdi zmdi-facebook"></i></a></li>
-                                    <li><a href="#"><i class="zmdi zmdi-twitter"></i></a></li>
-                                    <li><a href="#"><i class="zmdi zmdi-skype"></i></a></li>
-                                    <li><a href="#"><i class="zmdi zmdi-linkedin"></i></a></li>
-                                    <li><a href="#"><i class="zmdi zmdi-pinterest"></i></a></li>
+                                    <li><a href="#"><i class="zmdi zmdi-instagram"></i></a></li>
+                                    <li><a href="#"><i class="zmdi zmdi-youtube"></i></a></li>
+                                    <li><a href="#"><i class="zmdi zmdi-whatsapp"></i></a></li>
                                 </ul>
                             </div>
                             <div class="hs-mainmenu-wrap d-none d-md-block">
                                 <nav class="mainmenu-nav">
                                     <ul class="main-menu">
-                                        <li class="drop"><a href="index.html">HOME</a></li>
-                                        <li><a href="about-us.html">ABOUT</a></li>
-                                        <li class="drop"><a href="#">pages</a>
-                                            <ul class="dropdown">
-                                                <li><a href="about-us.html">about us</a></li>
-                                                <li><a href="checkout.html">checkout</a></li>
-                                                <li><a href="contact-us.html">contact us</a></li>
-                                                <li><a href="gallery.html">gallery</a></li>
-                                                <li><a href="my-account.html">my account</a></li>
-                                                <li><a href="pricing.html">pricing</a></li>
-                                                <li><a href="services.html">services</a></li>
-                                                <li><a href="shop.html">shop</a></li>
-                                                <li><a href="shop-list.html">shop list</a></li>
-                                                <li><a href="shop-details.html">shop details</a></li>
-                                                <li><a href="shopping-cart.html">shopping cart</a></li>
-                                                <li><a href="team.html">team</a></li>
-                                                <li><a href="wishlist.html">wishlist</a></li>
-                                            </ul>
-                                        </li>
-                                        <li class="drop"><a href="blog.html">blog</a>
-                                            <ul class="dropdown">
-                                                <li><a href="blog.html">blog</a></li>
-                                                <li><a href="blog-right-sidebar.html">blog sidebar</a></li>
-                                                <li><a href="blog-details.html">blog details</a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="gallery.html">gallery</a></li>
-                                        <li><a href="contact-us.html">CONTACT</a></li>
+                                        <li><a href="index.php">Home</a></li>
+                                        <li><a href="about.php">About</a></li>
+                                        <li><a href="services.php">Services</a></li>
+                                        <li><a href="products.php">Products</a></li>
+                                        <li><a href="#">Appointment Booking</a></li>
+                                        <li><a href="gallery.php">Gallery</a></li>
+                                        <li><a href="blog.php">Blog</a></li>
+                                        <li><a href="contact.php">Contact</a></li>
                                     </ul>
                                 </nav>
                             </div>
@@ -81,38 +60,18 @@
             </div>
             <div class="mobile-menu-area d-block d-md-none">
                 <div class="fluid-container mobile-menu-container">
-                    <div class="mobile-logo"><a href="index.html"><img src="images/logo/2.png" alt="Mobile logo"></a></div>
+                    <div class="mobile-logo"><a href="index.php"><img src="images/logo/2.png" alt="Mobile logo"></a></div>
                     <div class="mobile-menu clearfix">
                         <nav id="mobile_dropdown">
                             <ul>
-                                <li><a href="index.html">Home</a></li>
-                                <li><a href="about-us.html">About us</a></li>
-                                <li><a href="blog.html">blog</a>
-                                    <ul>
-                                        <li><a href="blog.html">blog</a></li>
-                                        <li><a href="blog-right-sidebar.html">blog sidebar</a></li>
-                                        <li><a href="blog-details.html">blog details</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="#">Pages</a>
-                                    <ul>
-                                        <li><a href="about-us.html">about us</a></li>
-                                        <li><a href="checkout.html">checkout</a></li>
-                                        <li><a href="contact-us.html">contact us</a></li>
-                                        <li><a href="gallery.html">gallery</a></li>
-                                        <li><a href="my-account.html">my account</a></li>
-                                        <li><a href="pricing.html">pricing</a></li>
-                                        <li><a href="services.html">services</a></li>
-                                        <li><a href="shop.html">shop</a></li>
-                                        <li><a href="shop-list.html">shop list</a></li>
-                                        <li><a href="shop-details.html">shop details</a></li>
-                                        <li><a href="shopping-cart.html">shopping cart</a></li>
-                                        <li><a href="team.html">team</a></li>
-                                        <li><a href="wishlist.html">wishlist</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="gallery.html">gallery</a></li>
-                                <li><a href="contact-us.html">CONTACT</a></li>
+                                <li><a href="index.php">Home</a></li>
+                                <li><a href="about.php">About</a></li>
+                                <li><a href="services.php">Services</a></li>
+                                <li><a href="products.php">Products</a></li>
+                                <li><a href="#">Appointment Booking</a></li>
+                                <li><a href="gallery.php">Gallery</a></li>
+                                <li><a href="blog.php">Blog</a></li>
+                                <li><a href="contact.php">Contact</a></li>
                             </ul>
                         </nav>
                     </div>  
