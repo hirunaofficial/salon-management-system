e54399b35454a59bf7e8f4bab282f766034613a9
GD Hiruna <hirunaofficial@gmail.com>
2024-10-02 02:07:58 +0530

    Add wishlist.php to handle wishlist functionality, allowing users to add products to their wishlist

--- wishlist.php
+++ wishlist.php
@@ -1,96 +1,135 @@
-<?php include 'header.php' ?>
+<?php
+include 'header.php';
+include 'dbconnect.php';
 
-        <section class="breadcrumbs-area ptb-100 bg-gray">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 text-center">
-                        <div class="breadcrumbs">
-                            <h2 class="page-title">wishlist</h2>
-                            <ul>
-                                <li>
-                                    <a class="active" href="#">Home</a>
-                                </li>
-                                <li>wishlist</li>
-                            </ul>
-                        </div>
-                    </div>
+// Ensure the user is logged in
+if (!isset($_SESSION['user_id'])) {
+    echo "<script type='text/javascript'>
+            window.location.href = 'login.php';
+          </script>";
+    exit;
+}
+
+// Handle adding product to wishlist
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['wishlist'])) {
+    $product_id = $_POST['product_id'];
+    $qty = isset($_POST['qty']) ? $_POST['qty'] : 1;
+    $user_id = $_SESSION['user_id'];
+
+    // Check if the product is already in the wishlist
+    $stmt = $pdo->prepare("SELECT * FROM wishlist WHERE user_id = :user_id AND product_id = :product_id");
+    $stmt->execute(['user_id' => $user_id, 'product_id' => $product_id]);
+    $wishlist_item = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($wishlist_item) {
+        // Update the quantity if the product is already in the wishlist
+        $stmt = $pdo->prepare("UPDATE wishlist SET qty = qty + :qty WHERE wishlist_id = :wishlist_id");
+        $stmt->execute(['qty' => $qty, 'wishlist_id' => $wishlist_item['wishlist_id']]);
+        $message = "Product quantity updated in the wishlist!";
+    } else {
+        // Insert new product into the wishlist
+        $stmt = $pdo->prepare("
+            INSERT INTO wishlist (user_id, product_id, qty)
+            VALUES (:user_id, :product_id, :qty)
+        ");
+        $stmt->execute([
+            'user_id' => $user_id,
+            'product_id' => $product_id,
+            'qty' => $qty
+        ]);
+        $message = "Product added to the wishlist!";
+    }
+}
+
+// Fetch wishlist items for the logged-in user
+$user_id = $_SESSION['user_id'];
+$stmt = $pdo->prepare("
+    SELECT w.wishlist_id, p.product_id, p.product_name, p.price, w.qty
+    FROM wishlist w
+    JOIN products p ON w.product_id = p.product_id
+    WHERE w.user_id = :user_id
+");
+$stmt->execute(['user_id' => $user_id]);
+$wishlist_items = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// Handle wishlist item removal
+if (isset($_GET['remove'])) {
+    $wishlist_id = $_GET['remove'];
+    $stmt = $pdo->prepare("DELETE FROM wishlist WHERE wishlist_id = :wishlist_id AND user_id = :user_id");
+    $stmt->execute([
+        'wishlist_id' => $wishlist_id,
+        'user_id' => $user_id
+    ]);
+    echo "<script type='text/javascript'>
+            window.location.href = 'wishlist.php';
+          </script>";
+    exit;
+}
+?>
+
+<section class="breadcrumbs-area ptb-100 bg-gray">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <div class="breadcrumbs">
+                    <h2 class="page-title">Wishlist</h2>
+                    <ul>
+                        <li><a class="active" href="index.php">Home</a></li>
+                        <li>Wishlist</li>
+                    </ul>
                 </div>
             </div>
-        </section>
+        </div>
+    </div>
+</section>
 
-        <div class="shopping-cart-area pt-90 pb-50">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12 col-xs-12">
-                        <div class="s-cart-all">
-                            <div class="page-title">
-                                <h1>wishlist :</h1>
-                            </div>
-                            <div class="cart-form table-responsive ma">
-                                <table id="shopping-cart-table" class="data-table cart-table">
-                                    <tr>
-                                        <th>Images</th>
-                                        <th>Product Name</th>
-                                        <th>Model</th>
-                                        <th>Qty</th>
-                                        <th>Unit Price</th>
-                                        <th>Total</th>
-                                        <th>column_remove</th>
-                                    </tr>
-                                    <tr>
-                                        <td class="sop-cart">
-                                            <a href="#"><img class="primary-image" alt="" src="images/shop/c1.jpg"></a>
-                                        </td>
-                                        <td class="sop-cart"><a href="#">adipiscing elit</a></td>
-                                        <td class="sop-cart">Product 3</td>
-                                        <td class="cen">
-                                            <input class="input-text qty" type="text" name="qty" maxlength="12" value="1" title="Qty">
-                                            <div class="tas ce-ta">
-                                                <a class="add" href="#/">
-                                                    <i class="zmdi zmdi-close-circle"></i>
-                                                </a>
-                                            </div>
-                                        </td>
-                                        <td class="sop-cart">$200.00</td>
-                                        <td class="sop-cart">$200.00</td>
-                                        <td class="sop-icon">
-                                            <div class="tas">
-                                                <a class="add" href="#/">
-                                                    <i class="zmdi zmdi-close-circle"></i>
-                                                </a>
-                                            </div>
-                                        </td>
-                                    </tr>
+<div class="shopping-cart-area pt-90 pb-50">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-xs-12">
+                <div class="s-cart-all">
+                    <div class="page-title">
+                        <h1>Wishlist :</h1>
+                    </div>
+
+                    <?php if (isset($message)): ?>
+                        <div class="alert alert-success"><?= $message ?></div>
+                    <?php endif; ?>
+
+                    <div class="cart-form table-responsive ma">
+                        <table id="shopping-cart-table" class="data-table cart-table">
+                            <tr>
+                                <th>Product Name</th>
+                                <th>Price</th>
+                                <th>Remove</th>
+                            </tr>
+                            <?php if (!empty($wishlist_items)): ?>
+                                <?php foreach ($wishlist_items as $item): ?>
                                     <tr>
                                         <td class="sop-cart">
-                                            <a href="#"><img class="primary-image" alt="" src="images/shop/c2.jpg"></a>
+                                            <a href="products.php">
+                                                <?= $item['product_name'] ?>
+                                            </a>
                                         </td>
-                                        <td class="sop-cart"><a href="#"> volutpat Reward</a></td>
-                                        <td class="sop-cart"><a href="#">Product 8</a></td>
-                                        <td class="cen">
-                                            <input class="input-text qty" type="text" name="qty" maxlength="12" value="1" title="Qty">
-                                            <div class="tas ce-ta">
-                                                <a class="add" href="#/">
-                                                    <i class="zmdi zmdi-close-circle"></i>
-                                                </a>
-                                            </div>
-                                        </td>
-                                        <td class="sop-cart">$110.00</td>
-                                        <td class="sop-cart">$110.00</td>
+                                        <td class="sop-cart">LKR <?= number_format($item['price'], 2) ?></td>
                                         <td class="sop-icon">
-                                            <div class="tas">
-                                                <a class="add" href="#/">
-                                                    <i class="zmdi zmdi-close-circle"></i>
-                                                </a>
-                                            </div>
+                                            <a href="wishlist.php?remove=<?= $item['wishlist_id'] ?>" class="remove">
+                                                <i class="zmdi zmdi-close-circle"></i>
+                                            </a>
                                         </td>
                                     </tr>
-                                </table>
-                            </div>
-                        </div>
+                                <?php endforeach; ?>
+                            <?php else: ?>
+                                <tr>
+                                    <td colspan="5" class="text-center">Your wishlist is empty.</td>
+                                </tr>
+                            <?php endif; ?>
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+</div>
 
 <?php include 'footer.php' ?>
