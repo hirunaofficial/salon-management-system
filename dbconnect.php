b9f878a03a9f93e374ca6fa6d748a675313351e5
GD Hiruna <hirunaofficial@gmail.com>
2024-10-01 13:49:46 +0530

    Updated dbconnect.php to set default fetch mode to FETCH_ASSOC

--- dbconnect.php
+++ dbconnect.php
@@ -7,6 +7,7 @@ $pass = '';
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
